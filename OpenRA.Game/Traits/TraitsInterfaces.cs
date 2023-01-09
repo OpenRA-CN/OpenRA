97651c35ea44606cace1aa64e210ce9e1174e9c5
penev92 <penev92@gmail.com>
2023-01-09 23:28:17 +0200

    Added ITilesetSpecificPaletteInfo for linting

--- OpenRA.Game/Traits/TraitsInterfaces.cs
+++ OpenRA.Game/Traits/TraitsInterfaces.cs
@@ -254,6 +254,12 @@ namespace OpenRA.Traits
 	}
 
 	[RequireExplicitImplementation]
+	public interface ITilesetSpecificPaletteInfo : ITraitInfoInterface
+	{
+		string Tileset { get; }
+	}
+
+	[RequireExplicitImplementation]
 	public interface IProvidesCursorPaletteInfo : ITraitInfoInterface
 	{
 		string Palette { get; }
